03c130d3d597cc8ea7190f23f218e9386215ba95
Tech-A12 <tech-a12@Tech-A12s-Air.home>
2018-02-04 17:48:02 +0000

    Using keydown and keyup to display multiple pressed keys in real time

--- POCPs/app.js
+++ POCPs/app.js
@@ -16,9 +16,33 @@ $(document).ready(function(){
     //Seems to function in an identical way to keypress, including multiple calls when the button is held down. Concluding that I use either this function or keypress, not both.
 
     //onkeyup
-    $('#demo').keyup(function(event){
-      console.log('keyup working');
-      console.log(event.which);
-    })
+    // $('#demo').keyup(function(event){
+    //   console.log('keyup working');
+    //   console.log(event.which);
+    // })
     //Is only called when key is lifted up. Will be useful in differentiating difference between hit and hold notes.
   })
+
+var keys = {};
+
+$(document).keydown(function(event){
+    keys[event.which] = true; //keys[event.which] adds the key pressed to array keys
+    printKeys();
+});
+
+$(document).keyup(function(event){
+    delete keys[event.which];
+    printKeys();
+});
+
+function printKeys() {
+    var html = '';
+    for (var i in keys) {
+        //keys.hasOwnProperty(i) checks if i is a direct property of keys, or it has been inherited from elsewhere.
+        //If i is not a direct property of keys, it skips this iteration of the loop
+        // if (!keys.hasOwnProperty(i)) continue; //Continue breaks 1 iteration of the loop
+        //It works with this commented out. But will keep around incase it turns out to be needed to prevent bug
+        html += i + ', ';
+    }
+    $('#demo').html(html);
+}
