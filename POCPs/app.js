7e60edec3a56545d2aaba7b9d0de95a2ad0cf1c4
Tech-A12 <tech-a12@Tech-A12s-MacBook-Air.local>
2018-02-05 14:34:06 +0000

    Commented out code removed

--- POCPs/app.js
+++ POCPs/app.js
@@ -1,26 +1,5 @@
 $(document).ready(function(){
     console.log("Working");
-    // hideImg();
-    //onkeypress
-    // $('#demo').keypress(function(event){
-    //   console.log('keypress working');
-    //   console.log(event.which);
-    //   //This returns a number corresponding to what was pressed. Each key has an assigned number, though this must be checked on other machines soon.
-    // })
-
-    //onkeydown
-    // $('#demo').keydown(function(event){
-    //   console.log('keydown working');
-    //   console.log(event.which);
-    // })
-    //Seems to function in an identical way to keypress, including multiple calls when the button is held down. Concluding that I use either this function or keypress, not both.
-
-    //onkeyup
-    // $('#demo').keyup(function(event){
-    //   console.log('keyup working');
-    //   console.log(event.which);
-    // })
-    //Is only called when key is lifted up. Will be useful in differentiating difference between hit and hold notes.
     moveNote();
   })
 
@@ -30,36 +9,20 @@ var lastInput = "rygbp"; //Declaring the 'default' input as no keys pushed
 
 $(document).keydown(function(event){
   if (jQuery.inArray(event.which,acceptedKeys)!=-1) {
-    //console.log(event.which);
     //Takes the accepted keys array, and returns the index of the first arguement, or -1 if it is not found
     input[event.which] = true; //creates an object in input with event.which as its key
-    //console.log(input);
-    // printKeys();
-    // showImg();
-    //addPushed($("#red"));
     whatIsPushed(input);
   }
 });
 
 $(document).keyup(function(event){
   delete input[event.which];
-  // printKeys();
-  // hideImg();
-  //removePushed($("#red"));
   whatIsPushed(input);
 });
 
  function printKeys(text) {
    $('#demo').html("<p>" + text + "</p>");
 }
-
-// function hideImg(){
-//   $("#imgdemo").hide();
-// }
-//
-// function showImg(){
-//   $("#imgdemo").show();
-// }
 
 function addPushed(obj){
   obj.addClass("pushed");
@@ -71,14 +34,8 @@ function removePushed(obj){
 
 //function to determine what keys are pushed, and then add pushed class to the right keys
 function whatIsPushed(keys){
-  // console.log(keys);
-  // console.log(jQuery.inArray(71,keys));
-  // console.log(jQuery.inArray(71,keys)!=-1);
-  //if (jQuery.inArray(71,keys) != -1) {
-  //if (keys.hasOwnProperty(71)) {
   playerInput='';
   if (71 in keys){
-    //console.log("add red");
     addPushed($("#red"));
     if (lastInput[0]!='r') {
       playerInput += '>';
@@ -87,7 +44,6 @@ function whatIsPushed(keys){
     }
   } else {
     removePushed($("#red"));
-    //console.log("remove red");
     playerInput += 'r';
   }
   if (72 in keys){
@@ -123,10 +79,6 @@ function whatIsPushed(keys){
 }
 
 function moveNote(){
-  // $("#demomove").animate({
-  //   left:"+=1000px"
-  // },10000)
-
   var pos = 1;
     var interval = setInterval(frame,10);
 
