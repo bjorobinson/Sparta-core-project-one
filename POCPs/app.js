473f3353b3c90f51450f765c50a0bb5d67c346a5
Tech-A12 <tech-a12@Tech-A12s-MacBook-Air.local>
2018-02-06 12:13:59 +0000

    File cleanup by removing commented out code from HTML and JS

--- POCPs/app.js
+++ POCPs/app.js
@@ -2,28 +2,17 @@ $(document).ready(function(){
     //new Audio('../music/Queen_We_Will_Rock_You.mp3').play()
     console.log("Working");
     showScore();
-    // moveNote();
 
-    //interval is typically sent to the number of beats per second.
-    //HOWEVER. This would only allow for full notes. Songs require at least 1/2 notes, so it is 1/2 this interval further.
-    //We Will Rock You: 731.7 milliseconds per beat
-     // var timer = setInterval(timerLoop,365.85);
-     // function timerLoop(){
-     //   $("#linespawner").spawnLine();
-     // }
-
-     //Count in 1, 2, 3, 4
-     waitBar();
-     waitBar();
-     //The "base base snare" for 1 bar of we will rock you
-     spawnNote("red");
-     spawnNote("red");
-     spawnNote("green");
-     spawnRest();
-     spawnNote("red");
-     spawnNote("red");
-     spawnNote("green");
-     spawnRest();
+    //The Song itself
+    //This is going to be very fiddly as I try and line up to ensure sync;
+    //Pause before song actually start
+    waitBar();
+    waitBar();
+    //The "base base snare" for 1 bar of we will rock you
+    chorus();
+    chorus();
+    chorus();
+    chorus();
   })
 
 var input = {};
@@ -33,8 +22,8 @@ var acceptedKeys = [71,72,74,75,76]; //G,H,J,K,L
 var spawnIterator = 0;
 var checkIterator = 0;
 var globDelay = 0;
-var globDelayIt = 350; //This is the time inbetween each 1/2 beat in the song.
-var noteTime = 1000; //The time it will take a note to reach the top of the screen to the end zone
+var globDelayIt = 360; //This is the time inbetween each 1/2 beat in the song.
+var noteTime = 1050; //The time it will take a note to reach the top of the screen to the end zone
 var score = 0;
 
 //Music and Time-based functions
@@ -56,9 +45,21 @@ function waitBeat(){
 //Base function for the 'waiting' - it waits equivalent time for 1/2 a beat
 function spawnRest(){
   setTimeout(function(){
-    console.log("rest");
+    // console.log("rest");
   },globDelay)
   globDelay += globDelayIt;
+}
+
+//The function that spawns the 'base base snare' bar that's repeated through the song
+function chorus(){
+  spawnNote("red");
+  spawnNote("red");
+  spawnNote("green");
+  spawnRest();
+  spawnNote("red");
+  spawnNote("red");
+  spawnNote("green");
+  spawnRest();
 }
 
 function showScore(){
@@ -103,8 +104,6 @@ $(document).keydown(function(event){
     var keycode = event.which
     input[keycode] = true; //creates an object in input with event.which as its key
     addPushed($("#" + whatButton(keycode)));
-    // whatIsPushed(input);
-    // hasNote($("#" + whatButton(keycode)));
     hasNote(whatButton(keycode));
   }
 });
@@ -113,7 +112,6 @@ $(document).keyup(function(event){
   var keycode = event.which
   delete input[keycode];
   removePushed($("#" + whatButton(keycode)));
-  // whatIsPushed(input);
 });
 
 function addPushed(obj){
@@ -126,105 +124,23 @@ function removePushed(obj){
 
 //function to check if the arguement button has a note in it
 function hasNote(colour){
-  //by default, our arg is red
-  //check if boundaries contain an object of type 'rednote'
-  //console.log(colour);
   var obj = $("#"+colour);
-  //console.log(obj);
   var nextNote = $("#"+checkIterator);
-  //console.log(nextNote);
-  // console.log(nextNote.offset().top);
-  //console.log($("#" + whatButton(obj)));
   try{
-    if ((nextNote.offset().top > obj.offset().top-20) && obj.hasClass(colour) && nextNote.hasClass(colour)) {
-      //console.log("hit!");
+    if ((nextNote.offset().top > obj.offset().top-40) && obj.hasClass(colour) && nextNote.hasClass(colour)) {
       addScore();
       nextNote.remove();
-      // checkIterator += 1;
     } else {
       subtractScore();
-      //console.log("miss!");
     }
   }
   catch(err){
     console.log(err.message);
     subtractScore();
   }
-  // if ((nextNote.offset().top > obj.offset().top-20) && obj.hasClass(colour) && nextNote.hasClass(colour)) {
-  //   //console.log("hit!");
-  //   addScore();
-  //   nextNote.remove();
-  //   // checkIterator += 1;
-  // } else {
-  //   subtractScore();
-  //   //console.log("miss!");
-  // }
-  //if so, delete said rednote and console log it
-
 }
 
-//function to determine what keys are pushed, and then add pushed class to the right keys
-// function whatIsPushed(keys){
-//   playerInput='';
-//   if (71 in keys){
-//     addPushed($("#red"));
-//     if (lastInput[0]!='r') {
-//       playerInput += '>';
-//     } else{
-//       playerInput += 'R';
-//     }
-//   } else {
-//     removePushed($("#red"));
-//     playerInput += 'r';
-//   }
-//   if (72 in keys){
-//     addPushed($("#yellow"));
-//     if (lastInput[1]!='y') {
-//       playerInput += '>';
-//     } else{
-//       playerInput += 'Y';
-//     }
-//   } else {
-//     removePushed($("#yellow"));
-//     playerInput += 'y';
-//   }
-//   if (74 in keys){
-//     addPushed($("#green"));
-//     if (lastInput[2]!='g') {
-//       playerInput += '>';
-//     } else{
-//       playerInput += 'G';
-//     }
-//   } else {
-//     removePushed($("#green"));
-//     playerInput += 'g';
-//   }
-//   if (75 in keys){
-//     addPushed($("#blue"));
-//     if (lastInput[3]!='b') {
-//       playerInput += '>';
-//     } else{
-//       playerInput += 'B';
-//     }
-//   } else {
-//     removePushed($("#blue"));
-//     playerInput += 'b';
-//   }
-//   if (76 in keys){
-//     addPushed($("#purple"));
-//     if (lastInput[4]!='p') {
-//       playerInput += '>';
-//     } else{
-//       playerInput += 'P';
-//     }
-//   } else {
-//     removePushed($("#purple"));
-//     playerInput += 'p';
-//   }
-//   //printKeys(playerInput);
-//   lastInput = playerInput;
-//   return playerInput;
-// }
+
 
 function spawnNote(noteColour){
   setTimeout(function(){
@@ -232,12 +148,9 @@ function spawnNote(noteColour){
     $(newNote).addClass("note");
     $(newNote).addClass(noteColour);
     $(newNote).addClass(noteColour+"Note"); //sets the background colour and where it will spawn on the board
-    // $(newNote).attr("id",Math.random()); //Generates random number ID
     $(newNote).attr("id",spawnIterator);
     spawnIterator++;
     $(newNote).appendTo('#notefall');
-    //console.log('note spawned');
-    //console.log($(newNote));
     moveNote($(newNote));
   },globDelay);
   globDelay += globDelayIt;
@@ -251,6 +164,4 @@ function moveNote(newNote){
     newNote.remove();
     checkIterator += 1;
   });
-  //console.log("animating note");
-  //console.log(newNote.offset());
 }
