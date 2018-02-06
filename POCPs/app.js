6fe6a1acadbd3fcb78aad881a6b66b160cb76c39
Tech-A12 <tech-a12@Tech-A12s-MacBook-Air.local>
2018-02-06 10:41:24 +0000

    More reworking. Program now spawns 2 red notes 1 second apart, both of which are recognised when hit in circle, and removed at the end

--- POCPs/app.js
+++ POCPs/app.js
@@ -11,29 +11,57 @@ $(document).ready(function(){
      // }
 
      spawnNote("redNote");
+     spawnNote("redNote");
+     // spawnNote("greenNote");
   })
 
 var input = {};
 var acceptedKeys = [71,72,74,75,76]; //G,H,J,K,L
-var playerInput = 'rygbp';//Declaring the 'default' input as no keys pushed
-var lastInput = "rygbp";
+// var playerInput = 'rygbp';//Declaring the 'default' input as no keys pushed
+// var lastInput = "rygbp";
+var spawnIterator = 0;
+var checkIterator = 0;
+var globDelay = 0;
+
+//function to convert keycodes of valid button pushes into what colours they referred to.
+function whatButton(keyCodeIn){
+  switch (keyCodeIn) {
+    case 71:
+      return 'red'
+      break;
+    case 72:
+      return 'yellow'
+      break;
+    case 74:
+      return 'green'
+      break;
+    case 75:
+      return 'blue'
+      break;
+    case 76:
+      return 'purple'
+      break;
+    default:
+  }
+}
 
 $(document).keydown(function(event){
   if (jQuery.inArray(event.which,acceptedKeys)!=-1) {
     //Takes the accepted keys array, and returns the index of the first arguement, or -1 if it is not found
-    input[event.which] = true; //creates an object in input with event.which as its key
-    whatIsPushed(input);
+    var keycode = event.which
+    input[keycode] = true; //creates an object in input with event.which as its key
+    addPushed($("#" + whatButton(keycode)));
+    // whatIsPushed(input);
+    hasNote();
   }
 });
 
 $(document).keyup(function(event){
-  delete input[event.which];
-  whatIsPushed(input);
+  var keycode = event.which
+  delete input[keycode];
+  removePushed($("#" + whatButton(keycode)));
+  // whatIsPushed(input);
 });
-
- function printKeys(text) {
-   $('#demo').html("<p>" + text + "</p>");
-}
 
 function addPushed(obj){
   obj.addClass("pushed");
@@ -43,127 +71,109 @@ function removePushed(obj){
   obj.removeClass("pushed");
 }
 
-//function to determine what keys are pushed, and then add pushed class to the right keys
-function whatIsPushed(keys){
-  playerInput='';
-  if (71 in keys){
-    addPushed($("#red"));
-    if (lastInput[0]!='r') {
-      playerInput += '>';
-    } else{
-      playerInput += 'R';
-    }
+//function to check if the arguement button has a note in it
+function hasNote(){
+  //by default, our arg is red
+  //check if boundaries contain an object of type 'rednote'
+  var nextNote = $("#"+checkIterator);
+  // console.log(nextNote.offset().top);
+  if (nextNote.offset().top > $("#red").offset().top-20) {
+    console.log("hit!");
+    nextNote.remove();
+    // checkIterator += 1;
   } else {
-    removePushed($("#red"));
-    playerInput += 'r';
+    console.log("miss!");
   }
-  if (72 in keys){
-    addPushed($("#yellow"));
-    if (lastInput[1]!='y') {
-      playerInput += '>';
-    } else{
-      playerInput += 'Y';
-    }
-  } else {
-    removePushed($("#yellow"));
-    playerInput += 'y';
-  }
-  if (74 in keys){
-    addPushed($("#green"));
-    if (lastInput[2]!='g') {
-      playerInput += '>';
-    } else{
-      playerInput += 'G';
-    }
-  } else {
-    removePushed($("#green"));
-    playerInput += 'g';
-  }
-  if (75 in keys){
-    addPushed($("#blue"));
-    if (lastInput[3]!='b') {
-      playerInput += '>';
-    } else{
-      playerInput += 'B';
-    }
-  } else {
-    removePushed($("#blue"));
-    playerInput += 'b';
-  }
-  if (76 in keys){
-    addPushed($("#purple"));
-    if (lastInput[4]!='p') {
-      playerInput += '>';
-    } else{
-      playerInput += 'P';
-    }
-  } else {
-    removePushed($("#purple"));
-    playerInput += 'p';
-  }
-  //printKeys(playerInput);
-  lastInput = playerInput;
-  return playerInput;
+  //if so, delete said rednote and console log it
+
 }
 
-// function moveNote(){
-//   var pos = 0;
-//   var incrament = 4;
-//   var zoneLowerlimit = 700;
-//   var zoneUpperlimit = 800;
-//   var interval = setInterval(frame,10);
-//
-//   function frame(){
-//     if (pos < 0){
-//       clearInterval(interval);
-//     } else {
-//       pos += incrament;
-//       $('#demomove').css('margin-left', pos+ 'px');
-//       if (pos > zoneLowerlimit && pos < zoneUpperlimit && playerInput[0]=='R') {
-//         $('#demomove').remove();
-//       } else if (pos > zoneUpperlimit) {
-//         $('#demomove').remove();
-//       }
+//function to determine what keys are pushed, and then add pushed class to the right keys
+// function whatIsPushed(keys){
+//   playerInput='';
+//   if (71 in keys){
+//     addPushed($("#red"));
+//     if (lastInput[0]!='r') {
+//       playerInput += '>';
+//     } else{
+//       playerInput += 'R';
 //     }
+//   } else {
+//     removePushed($("#red"));
+//     playerInput += 'r';
 //   }
-//
-//   console.log("animating");
+//   if (72 in keys){
+//     addPushed($("#yellow"));
+//     if (lastInput[1]!='y') {
+//       playerInput += '>';
+//     } else{
+//       playerInput += 'Y';
+//     }
+//   } else {
+//     removePushed($("#yellow"));
+//     playerInput += 'y';
+//   }
+//   if (74 in keys){
+//     addPushed($("#green"));
+//     if (lastInput[2]!='g') {
+//       playerInput += '>';
+//     } else{
+//       playerInput += 'G';
+//     }
+//   } else {
+//     removePushed($("#green"));
+//     playerInput += 'g';
+//   }
+//   if (75 in keys){
+//     addPushed($("#blue"));
+//     if (lastInput[3]!='b') {
+//       playerInput += '>';
+//     } else{
+//       playerInput += 'B';
+//     }
+//   } else {
+//     removePushed($("#blue"));
+//     playerInput += 'b';
+//   }
+//   if (76 in keys){
+//     addPushed($("#purple"));
+//     if (lastInput[4]!='p') {
+//       playerInput += '>';
+//     } else{
+//       playerInput += 'P';
+//     }
+//   } else {
+//     removePushed($("#purple"));
+//     playerInput += 'p';
+//   }
+//   //printKeys(playerInput);
+//   lastInput = playerInput;
+//   return playerInput;
 // }
 
 function spawnNote(noteClass){
-  var newNote = document.createElement("div");
-  $(newNote).addClass("note");
-  $(newNote).addClass(noteClass); //sets the background colour and where it will spawn on the board
-  $(newNote).attr("id",Math.random()); //Generates random number ID
-  $(newNote).appendTo('#notefall');
-  console.log('note spawned');
-  moveNote($(newNote));
+  setTimeout(function(){
+    var newNote = document.createElement("div");
+    $(newNote).addClass("note");
+    $(newNote).addClass(noteClass); //sets the background colour and where it will spawn on the board
+    // $(newNote).attr("id",Math.random()); //Generates random number ID
+    $(newNote).attr("id",spawnIterator);
+    spawnIterator++;
+    $(newNote).appendTo('#notefall');
+    console.log('note spawned');
+    moveNote($(newNote));
+  },globDelay);
+  globDelay += 1000;
 }
 
 function moveNote(newNote){
-  // var pos = 0;
-  // var incrament = 1;
-  // var zoneLowerlimit = 700;
-  // var zoneUpperlimit = 800;
-  // var interval = setInterval(frame,10);
-  // function frame(){
-  //   if (pos < 0){
-  //     clearInterval(interval);
-  //   } else {
-  //     pos += incrament;
-  //     newNote.css('padding-top', pos+ 'px');
-  //     if (pos > zoneLowerlimit && pos < zoneUpperlimit && playerInput[0]=='R') {
-  //       newNote.remove();
-  //     } else if (pos > zoneUpperlimit) {
-  //       newNote.remove();
-  //     }
-  //   }
-  //}
   newNote.animate({
-    top:'200px'
+    top:'400px'
   },4000,"linear", function(){
     //excute when complete
-    console.log("complete");
+    newNote.remove();
+    checkIterator += 1;
   });
   console.log("animating note");
+  console.log(newNote.offset());
 }
